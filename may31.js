7940679a36fa7383d2ef114dad174d938ca2af72
edwardholder <holder.edward@heb.com>
2024-05-31 12:00:40 -0500

    Create may31.js

---
+++ may31.js
@@ -0,0 +1,9 @@
+/*
+caller_id:83c6cadc87cd8110f96c62483cbb3531
+category:software
+subcategory:email
+business_service:ebd9403187248d10cde6311d0ebb3546
+contact_type:email
+description: loss of all power
+*/
+const incGr = new GlideRecord('incident');
